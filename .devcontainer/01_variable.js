6ac159c44ffa496abbf56f1426ae3c9601cafc4f
Satishmirjan <123397923+Satishmirjan@users.noreply.github.com>
2024-02-03 14:29:22 +0000

    program to learn variables

---
+++ .devcontainer/01_variable.js
@@ -0,0 +1,7 @@
+let name="satish naik";
+const contact=7483160556;
+var cityName="karwar";
+
+console.log(`my name is ${name}  my contact number is ${contact}\n city:${cityName}`);
+cityName="Mysuru";
+console.log(`I chabged my city to ${cityName}`);
